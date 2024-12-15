592fa6b641fd54ede089a7315aa9b26209d3ce3c
Roy <143868022+07ReuelBodhak@users.noreply.github.com>
2024-12-15 14:21:34 +0530

    Update practical number 3

--- PR_5/main.py
+++ PR_5/main.py
@@ -3,7 +3,7 @@ def play_cricket_badminton(group_a,group_b):
 
 def play_not_cricket_badminton(group_a,group_b,group_c):
     play_both = group_a.union(group_b)
-    print(group_c.difference(play_both))
+    print(len(group_c.difference(play_both)))
 
 group_a = {"Alex", "Bob", "Charlie", "David"} #play cricket
 group_b = {"Eddie", "Frank", "Charlie", "George"} #play badminton
